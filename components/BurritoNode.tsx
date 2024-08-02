7a3f5349f0141f50ab9478fbefc445cc9d064db7
Yizhou Li <yizhoul316@hotmail.com>
2024-08-02 16:03:13 -0400

    feat: modify burrito node

--- components/BurritoNode.tsx
+++ components/BurritoNode.tsx
@@ -1,101 +1,57 @@
 'use client';
 
 import { Button, colors } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import SendIcon from '@mui/icons-material/Send';
 import RadarAnimation from './RadarAnimation';
-import CountUp from 'react-countup';
-import { useInView } from 'react-intersection-observer';
+import StakeDetail from './StakeDetail';
+
 import bg from '@/public/images/bg2.jpg';
 import bgStyles from '@/styles/Background.module.css';
 
 const BurritoNode = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1, // Adjust the threshold value as needed
-  });
-
-  const [daysSince, setDaysSince] = useState(0);
-
-  useEffect(() => {
-    const startDate = new Date('2024-07-11').getTime();
-    const today = new Date().getTime();
-    const timeDiff = today - startDate;
-    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    setDaysSince(daysDiff);
-  }, []);
-
   return (
     <section
       id="burrito-node"
-      className={`${bgStyles.bgImage} ${bgStyles.overlay} py-10 px-10 text-center sm:p-20 sm:px-60 flex flex-col sm:flex-row sm:justify-around animate-fadeIn`}
+      className={`${bgStyles.bgImage} ${bgStyles.overlay}`}
       style={{ backgroundImage: `url(${bg.src})` }}
     >
       <div
-        className={`${bgStyles.content} max-w-xl`}
-        data-aos="zoom-y-out"
-        data-aos-delay={150}
+        className={` py-10 px-10 text-center sm:p-10 sm:px-60 flex flex-col sm:flex-row sm:justify-around `}
       >
-        <div className="text-5xl p-8 font-sans font-bold tracking-wide">
-          Burrito Node
+        <div
+          className={`${bgStyles.content} max-w-xl`}
+          data-aos="zoom-y-out"
+          data-aos-delay={150}
+        >
+          <div className="text-5xl p-8 font-sans font-bold tracking-wide">
+            Burrito Node
+          </div>
+          <div className="text-lg leading-relaxed">
+            Burrito Node is a high-security, non-custodial validator on the
+            Terra Classic blockchain, dedicated to maintaining high uptime and
+            ensuring the seamless operation of the network. Our node is designed
+            to support the Burrito Money project while contributing to the
+            overall stability and security of the Terra Classic ecosystem.
+          </div>
         </div>
-        <div className="text-lg leading-relaxed">
-          Burrito Node is a high-security, non-custodial validator on the Terra
-          Classic blockchain, dedicated to maintaining high uptime and ensuring
-          the seamless operation of the network. Our node is designed to support
-          the Burrito Money project while contributing to the overall stability
-          and security of the Terra Classic ecosystem.
-        </div>
-        <ul>
-          <li className="pt-5 text-xl font-semibold text-green-600">
-            Low Commission Fee
-          </li>
-          <li className="pt-5 text-xl font-semibold text-green-600">
-            High Stability
-          </li>
-        </ul>
-        <div className="py-10 animate-bounce">
-          <Button
-            variant="contained"
-            endIcon={<SendIcon />}
-            className="bg-blue-600 rounded-full shadow-lg mt-5"
-            onClick={() =>
-              window.open(
-                'https://station.terra.money/validator/terravaloper16x9dcx9pm9j8ykl0td4hptwule706ysjel6500',
-                '_blank',
-              )
-            }
-          >
-            Stake With Us
-          </Button>
-        </div>
-      </div>
-      <div className={`${bgStyles.content} flex-col`}>
         <RadarAnimation />
-        <div className="flex-col items-center md:flex-row " ref={ref}>
-          <div>
-            <div className="text-xl p-5">Total Stake</div>
-            <div className="flex justify-center px-5">
-              {inView && (
-                <div className="text-3xl font-semibold">
-                  <CountUp end={20234697703} />
-                </div>
-              )}
-              <span className="flex items-center pl-3 text-lg">Lunc</span>
-            </div>
-          </div>
-          <div>
-            <div className="text-xl p-5">Onlined</div>
-            <div className="flex justify-center px-5">
-              {inView && (
-                <div className="text-3xl font-semibold">
-                  <CountUp end={daysSince} />
-                </div>
-              )}
-              <span className="flex items-center pl-3 text-lg">days</span>
-            </div>
-          </div>
-        </div>
+      </div>
+      <StakeDetail />
+      <div className="py-10 animate-bounce text-center mt-5">
+        <Button
+          variant="contained"
+          endIcon={<SendIcon />}
+          className="bg-blue-600 rounded-full shadow-lg"
+          onClick={() =>
+            window.open(
+              'https://station.terra.money/validator/terravaloper16x9dcx9pm9j8ykl0td4hptwule706ysjel6500',
+              '_blank',
+            )
+          }
+        >
+          Stake With Us
+        </Button>
       </div>
     </section>
   );
---
+++ components/StakeDetail.tsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import bg from '@/public/images/bg2.jpg';
+import bgStyles from '@/styles/Background.module.css';
+import { useInView } from 'react-intersection-observer';
+import CountUp from 'react-countup';
+import { Divider, useMediaQuery, useTheme } from '@mui/material';
+import FileCopyOutlinedIcon from '@mui/icons-material/FileCopyOutlined';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Tooltip } from '@mui/material';
+
+const StakeDetail = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1, // Adjust the threshold value as needed
+  });
+
+  const [copied, setCopied] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [daysSince, setDaysSince] = useState(0);
+
+  const fullAddress = 'terravaloper16x9dcx9pm9j8ykl0td4hptwule706ysjel6500';
+  const [displayAddress, setDisplayAddress] = useState(fullAddress);
+
+  useEffect(() => {
+    const startDate = new Date('2024-07-11').getTime();
+    const today = new Date().getTime();
+    const timeDiff = today - startDate;
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    setDaysSince(daysDiff);
+
+    if (isMobile) {
+      setDisplayAddress('terravaloper.....jel6500');
+    } else {
+      setDisplayAddress(fullAddress);
+    }
+  }, [isMobile]);
+
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
+  };
+
+  return (
+    <>
+      <div
+        className="max-w-xs mx-auto sm:max-w-4xl bg-gray-900 rounded-2xl "
+        data-aos="zoom-y-out"
+        data-aos-delay={150}
+      >
+        <div className={`${bgStyles.content} p-4`}>
+          <div className="flex flex-col items-center md:flex-row" ref={ref}>
+            <div className="w-full dm:w-80 p-3">
+              <div className="text-xl p-5 mx-auto text-center">Total Stake</div>
+              <div className="flex justify-center mx-auto p-5">
+                {inView && (
+                  <div className="text-3xl font-semibold text-green-500">
+                    <CountUp end={20234697703} />
+                  </div>
+                )}
+                <span className="flex items-center pl-3 text-lg">Lunc</span>
+              </div>
+            </div>
+            <Divider
+              orientation={isMobile ? 'horizontal' : 'vertical'}
+              variant="middle"
+              flexItem
+              sx={{ bgcolor: 'white' }}
+            />
+            <div className="w-full dm:w-80 p-3">
+              <div className="text-xl p-5 mx-auto text-center">Onlined</div>
+              <div className="flex justify-center p-5">
+                {inView && (
+                  <div className="text-3xl font-semibold">
+                    <CountUp end={daysSince} />
+                  </div>
+                )}
+                <span className="flex items-center pl-3 text-lg">days</span>
+              </div>
+            </div>
+            <Divider
+              orientation={isMobile ? 'horizontal' : 'vertical'}
+              variant="middle"
+              flexItem
+              sx={{ bgcolor: 'white' }}
+            />
+            <div className="w-full dm:w-80 p-3">
+              <div className="text-xl p-5 mx-auto text-center">Commision</div>
+              <div className="flex justify-center p-5">
+                {inView && (
+                  <div className="text-3xl font-semibold">
+                    <CountUp end={3.11} decimals={2} suffix=" %" />
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="p-5 md:mt-5 text-center">
+            <div className="text-2xl bg-">Validator address </div>
+            <div className="bg-gray-600 inline-flex items-center rounded-lg mt-4">
+              <span className="text-white py-2 px-4">{displayAddress}</span>
+            </div>
+            <CopyToClipboard text={fullAddress} onCopy={handleCopy}>
+              <Tooltip title={copied ? 'Copied!' : 'Copy'}>
+                <FileCopyOutlinedIcon className="ml-4 text-white cursor-pointer" />
+              </Tooltip>
+            </CopyToClipboard>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default StakeDetail;
